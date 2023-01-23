e57b832788ffed37e8d47150cd9bc3c405f6f7ab
Franco Urrutia <francosusfu12@gmail.com>
2023-01-23 10:08:42 -0300

    Diseños Flexbox

---
+++ app.js
@@ -0,0 +1,18 @@
+const express= require("express")
+const app = express()
+const path = require("path")
+const PORT = 3000
+
+app.use(express.static("public"));
+
+
+app.get("/",(req,res)=> {
+  return res.sendFile(path.join(__dirname,"/view/home.html"))
+})
+
+
+
+app.listen(3000,()=>{console.log(
+`Server listen in port ${PORT}
+http://localhost:${PORT}
+`)})
